71463fb475f7bb0d8aea68ff282d4e322506ebfa
Bhrema <brunohcrema@gmail.com>
2023-11-13 08:38:12 -0300

    InsertSort criado

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,2 +1,29 @@
-package algoritmos;public class InsertionSort {
+package algoritmos;
+
+public class InsertionSort implements AlgoritmoOrdencao {
+    @Override
+    public void ordenacao(int[] array) {
+
+        System.out.println("-------------------- INSERTION SORT ---------------------");
+        for (int indiceAtual = 1; indiceAtual < array.length; ++indiceAtual) {
+            int chave = array[indiceAtual];
+            int indiceAnterior = indiceAtual - 1;
+
+            while (indiceAnterior >= 0 && array[indiceAnterior] > chave) {
+                array[indiceAnterior + 1] = array[indiceAnterior];
+                indiceAnterior = indiceAnterior - 1;
+            }
+            array[indiceAnterior + 1] = chave;
+
+            System.out.println("Após a iteração " + indiceAtual + ":");
+            impressao(array);
+        }
+    }
+
+    public void impressao(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
